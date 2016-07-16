1c1d39e07b0afb5904f4157ef9ba866571a0126c
evializ1 <evializ1@users.noreply.github.com>
2016-07-16 13:33:09 -0400

    Update weather.rb
    Added code for current (today) weather

--- weather.rb
+++ weather.rb
@@ -1,16 +1,20 @@
 require 'barometer'
 
-puts "What city are you in?"
- location = gets.capitalize
- 
-  def get_location_weather(location)
+	puts "What city are you in?"
+	location = gets.capitalize
+	
+	def get_location_weather(location)
 	 barometer = Barometer.new(location).measure
 	 end
 	weather = get_location_weather(location)
+	 today = Time.new.day
 	 tomorrow = Time.now.strftime('%d').to_i + 1
 	weather.forecast.each do |forecast|
-	 day = forecast.starts_at.day
-	 	if day ==tomorrow
+	 
+	day = forecast.starts_at.day
+	if day ==today
+	dayName = 'Today'
+	elsif day ==tomorrow
 	 dayName = 'Tomorrow'
 	else
 	 dayName = forecast.starts_at.strftime('%A')
